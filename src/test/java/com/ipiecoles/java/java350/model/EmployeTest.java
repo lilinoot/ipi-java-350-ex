9863907298c2d3feb8ba755634cde5a5f9924c9c
Lili <lili.vernet@gmail.com>
2019-03-14 15:41:10 +0100

    Tests sur le nombre d'années d'ancienneté

--- src/test/java/com/ipiecoles/java/java350/model/EmployeTest.java
+++ src/test/java/com/ipiecoles/java/java350/model/EmployeTest.java
@@ -8,7 +8,7 @@ import java.time.LocalDate;
 public class EmployeTest {
     @Test
 
-    public void testGetNombreAnneeAncienneteNow () {
+    public void testGetNombreAnneeAncienneteNow() {
         // Given
         Employe employe = new Employe(); // J'initialise un employé
         LocalDate dateEmbauche = LocalDate.now(); // Je déclare la date d'embauche à maintenant
@@ -18,6 +18,42 @@ public class EmployeTest {
         Integer nbAnnee = employe.getNombreAnneeAnciennete(); // Je récupère le nombre d'années dans une variable avec la méthode. Cette méthode repose sur une instance donc on doit instancier employé dans les données d'entrée
 
         // Then
-        Assertions.assertThat(nbAnnee).isEqualTo(0); // Je vérifie que cette date soit égale à la valeur attendue
+        Assertions.assertThat(nbAnnee).isEqualTo(0); // Quelqu'un qui est embauché maintenant, on doit donc avoir 0
+    }
+
+    public void testGetNombreAnneeAncienneteIsNull() {
+        // Given
+        Employe e = new Employe();
+        e.setDateEmbauche(null);
+
+        // When
+        Integer nbAnnee = e.getNombreAnneeAnciennete();
+
+        // Then
+        Assertions.assertThat(nbAnnee).isEqualTo(0);
+    }
+
+    public void testGetNombreAnneeAncienneteNMoins2() {
+        // Given
+        Employe e = new Employe();
+        e.setDateEmbauche(LocalDate.now().minusYears(2));
+
+        // When
+        Integer nbAnnee = e.getNombreAnneeAnciennete();
+
+        // Then
+        Assertions.assertThat(nbAnnee).isEqualTo(2);
+    }
+
+    public void testGetNombreAnneeAncienneteNPlus2() {
+        // Given
+        Employe e = new Employe();
+        e.setDateEmbauche(LocalDate.now().plusYears(2));
+
+        // When
+        Integer nbAnnee = e.getNombreAnneeAnciennete();
+
+        // Then
+        Assertions.assertThat(nbAnnee).isEqualTo(0);
     }
 }
