00f7c14b5b2cf881bd13c1af00b25f40812ce514
Lili <lili.vernet@gmail.com>
2019-04-07 16:20:31 +0200

    Version définitive

--- src/test/java/com/ipiecoles/java/java350/repository/EmployeRepositoryTest.java
+++ src/test/java/com/ipiecoles/java/java350/repository/EmployeRepositoryTest.java
@@ -60,7 +60,7 @@ public class EmployeRepositoryTest {
     @Test
     public void integrationAvgPerformanceWhereMatriculeStartsWith() {
         //Given
-        employeRepository.save(new Employe("Doe", "John", "C12345", LocalDate.now(), Entreprise.SALAIRE_BASE, 2, 1.0));
+        employeRepository.save(new Employe("Wells", "Andrew", "C12345", LocalDate.now(), Entreprise.SALAIRE_BASE, 2, 1.0));
         employeRepository.save(new Employe("Harris", "Xander", "C12346", LocalDate.now(), Entreprise.SALAIRE_BASE, 1, 1.0));
         employeRepository.save(new Employe("Giles", "Rupert", "C12347", LocalDate.now(), Entreprise.SALAIRE_BASE, 4, 1.0));
         employeRepository.save(new Employe("Rosenberg", "Willow", "C12348", LocalDate.now(), Entreprise.SALAIRE_BASE, 3, 1.0));
